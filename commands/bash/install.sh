9a172bcf4f3d78bb296316daf9a3ec8ca6935f45
Jason D'Souza <jason.rgd@gmail.com>
2019-11-01 18:07:40 +1100

    folder structure

---
+++ commands/bash/install.sh
@@ -0,0 +1,7 @@
+#install as a global command and add ./bin to user/system path
+
+#check to see if configuration manager can be installed
+
+#install configuration manager
+
+#install all dependencies to make this work by running the playbook
