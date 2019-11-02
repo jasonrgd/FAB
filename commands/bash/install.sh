45fced04e99e4785c01b002fc955b4f292c3a54a
Jason D'Souza <jason.rgd@gmail.com>
2019-11-02 22:54:42 +1100

    install command pre-requisites working

--- commands/bash/install.sh
+++ commands/bash/install.sh
@@ -7,25 +7,8 @@ trap '>&2 printf "\n\e[01;31mERROR\033[0m: Command \`%s\` on line $LINENO failed
 #install as a global command and add ./bin to user/system path
 
 #check to see if configuration manager can be installed
-
-python_installed="false"
-pip_installed="false"
-
-## check to see if python is installed or not
-if [ command -v python3 &>/dev/null ]; then
-    echo "py"
-    python_installed="true"
-fi
-if  [ command -v pip3 &>/dev/null ]; then
-    echo "pip"
-    pip_installed="true"
-fi
-
-echo ${python_installed}, ${pip_installed}
-if [[ pip_installed == "false"  || python_installed == "false" ]]; then
-    echo "je;"
-    fab_install_package python3 python3-pip
-fi
+fab_install_package python3 python3
+fab_install_package pip3 python3-pip
 
 #install configuration manager
 #python3 -m pip install ansible
--- commands/bash/library/install_package.sh
+++ commands/bash/library/install_package.sh
@@ -7,12 +7,21 @@ trap '>&2 printf "\n\e[01;31mERROR\033[0m: Command \`%s\` on line $LINENO failed
 fab_install_package()
 {
     declare OS="$(uname -s)"
+    declare check_installed=`command -v $1`
+    if [[ ${check_installed} ]]; then
+        install=false
+    else
+        echo "installing $2"
+        install=true 
+    fi
 
-    if [ "${OS}" == "Darwin" ]; then
-        brew install -y $@
-    elif [ "${OS}" == "Linux" ]; then
-        if [ -f /etc/debian_version ]; then
-            sudo apt-get install -y $@
+    if [[ ${install} == true ]]; then
+        if [ "${OS}" == "Darwin" ]; then
+            brew install -y $2
+        elif [ "${OS}" == "Linux" ]; then
+            if [ -f /etc/debian_version ]; then
+                sudo apt-get install -y $2
+            fi
         fi
     fi
 }
