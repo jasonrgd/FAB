bc30f901ebcae6aef548c30e28d6f4810701267b
Jason D'Souza <jason.rgd@gmail.com>
2019-11-02 23:52:59 +1100

    added ability to install ansible if not installed

--- commands/bash/install.sh
+++ commands/bash/install.sh
@@ -6,11 +6,16 @@ trap '>&2 printf "\n\e[01;31mERROR\033[0m: Command \`%s\` on line $LINENO failed
 
 #install as a global command and add ./bin to user/system path
 
+
 #check to see if configuration manager can be installed
 fab_install_package python3 python3
 fab_install_package pip3 python3-pip
 
+python=python3
+
 #install configuration manager
-#python3 -m pip install ansible
+if ! $python -c 'import pkgutil; exit(not pkgutil.find_loader("ansible"))'; then
+    $python -m pip install ansible
+fi
 
 #install all dependencies to make this work by running the playbook
